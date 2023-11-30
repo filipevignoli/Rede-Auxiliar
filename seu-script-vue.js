6eb3db219c72ab03f8a527774e86a326392240a9
filipevignoli <152338573+filipevignoli@users.noreply.github.com>
2023-11-29 21:02:02 -0300

    Add files via upload

---
+++ seu-script-vue.js
@@ -0,0 +1,23 @@
+new Vue({
+    el: '#dataTable',
+    data: {
+        usuarios: [
+            { id: 1, Especialidade: 'A', Subespecialidade: 'B', Profissional: 'C', Código: 'D', Prestador: 'E', Bairro: 'F', Município: 'G', Idade: 'H', Observações: ''	},
+            { id: 1, Especialidade: 'A', Subespecialidade: 'B', Profissional: 'C', Código: 'D', Prestador: 'E', Bairro: 'F', Município: 'G', Idade: 'H', Observações: ''	},
+            { id: 1, Especialidade: 'A', Subespecialidade: 'B', Profissional: 'C', Código: 'D', Prestador: 'E', Bairro: 'F', Município: 'G', Idade: 'H', Observações: ''	},
+            { id: 1, Especialidade: 'A', Subespecialidade: 'B', Profissional: 'C', Código: 'D', Prestador: 'E', Bairro: 'F', Município: 'G', Idade: 'H', Observações: ''	},
+            
+            
+
+
+
+
+
+            
+            
+            
+            // Adicione mais usuários conforme necessário
+        ]
+    }
+});
+    
