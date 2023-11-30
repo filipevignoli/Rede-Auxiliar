1da15f6372147d2f047506876fe579fb3423112d
Rede Auxiliar <152338573+filipevignoli@users.noreply.github.com>
2023-11-29 21:07:24 -0300

    Add files via upload

--- seu-script-vue.js
+++ seu-script-vue.js
@@ -2,10 +2,10 @@ new Vue({
     el: '#dataTable',
     data: {
         usuarios: [
-            { id: 1, Especialidade: 'A', Subespecialidade: 'B', Profissional: 'C', Código: 'D', Prestador: 'E', Bairro: 'F', Município: 'G', Idade: 'H', Observações: ''	},
-            { id: 1, Especialidade: 'A', Subespecialidade: 'B', Profissional: 'C', Código: 'D', Prestador: 'E', Bairro: 'F', Município: 'G', Idade: 'H', Observações: ''	},
-            { id: 1, Especialidade: 'A', Subespecialidade: 'B', Profissional: 'C', Código: 'D', Prestador: 'E', Bairro: 'F', Município: 'G', Idade: 'H', Observações: ''	},
-            { id: 1, Especialidade: 'A', Subespecialidade: 'B', Profissional: 'C', Código: 'D', Prestador: 'E', Bairro: 'F', Município: 'G', Idade: 'H', Observações: ''	},
+            { id: 1, Especialidade: 'A', Subespecialidade: 'A', Profissional: 'A', Código: 'A', Prestador: 'A', Bairro: 'A', Município: 'A', Idade: 'A', Observações: ''	},
+            { id: 1, Especialidade: 'B', Subespecialidade: 'B', Profissional: 'B', Código: 'B', Prestador: 'B', Bairro: 'B', Município: 'B', Idade: 'B', Observações: ''	},
+            { id: 1, Especialidade: 'C', Subespecialidade: 'C', Profissional: 'C', Código: 'C', Prestador: 'C', Bairro: 'C', Município: 'C', Idade: 'C', Observações: ''	},
+            { id: 1, Especialidade: 'D', Subespecialidade: 'D', Profissional: 'D', Código: 'D', Prestador: 'D', Bairro: 'D', Município: 'D', Idade: 'D', Observações: ''	},
             
             
 
